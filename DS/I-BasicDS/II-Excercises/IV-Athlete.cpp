8adc99048f01eb6b7783a21542bb2440525b2570
Axel3246 <hplaxel@hotmail.com>
2021-06-22 13:00:57 -0500

    DS - Introduction to DS
    Practicing DS for next semester and coding challenges.

---
+++ DS/I-BasicDS/II-Excercises/IV-Athlete.cpp
@@ -0,0 +1,6 @@
+//TODO: Realizar un programa que lea un arreglo de estructuras los datos de N empleados de la empresa
+//TODO: y que imprima los datos del empleado con mayor y menor salario
+
+#include <iostream>
+#include <string>
+using namespace std;
