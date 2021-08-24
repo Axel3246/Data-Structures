be5da6f2dfc2a90e0c36e73a95fc9571645e6e4d
Axel3246 <hplaxel@gmail.com>
2021-08-23 21:47:49 -0500

    Update 4-SpiralMatrix.cpp

--- LeetCode/4-SpiralMatrix.cpp
+++ LeetCode/4-SpiralMatrix.cpp
@@ -14,7 +14,7 @@ public:
     vector<int> spiralOrder(vector<vector<int>> &matrix)
     {
         // * Variables que nos indicaran la direccion y posicion de los valores
-        int m = matrix.size(), n = matrix.size() - 1, techomtrx = 0, pisomtrx = m - 1, izqMtrx = 0, derMtrx = n - 1, indx = 0;
+        int m = matrix.size(), n = m ? matrix[0].size() : 0, techomtrx = 0, pisomtrx = m - 1, izqMtrx = 0, derMtrx = n - 1, indx = 0;
 
         // * Se crea una matriz en forma de vector
         vector<int> matriz(m * n);
@@ -29,7 +29,7 @@ public:
                 matriz[indx++] = matrix[techomtrx][column];
             }
             // * Asegurando que no se rompan los limites de la matriz
-            if (++techomtrx >= derMtrx)
+            if (++techomtrx > pisomtrx)
             {
 
                 break;
@@ -45,11 +45,25 @@ public:
             {
                 break;
             }
-            // * Ahora pasamos a la columna derecha, la cual deberia tomar el valor derecho de en medio 
-            for (int columna = derMtrx; columna >= 1; --columna){
-                matriz[indx++] = matrix[derMtrx][columna];
+            // * Ahora pasamos a la columna derecha, la cual deberia tomar el valor derecho de en medio
+            for (int columna = derMtrx; columna >= izqMtrx; columna--)
+            {
+                matriz[indx++] = matrix[pisomtrx][columna];
             }
-            if()
+            // * Cambio de direccion
+            if (--pisomtrx < techomtrx)
+            {
+                break;
+            }
+            for (int fila = pisomtrx; fila >= techomtrx; fila--)
+            {
+                matriz[indx++] = matrix[fila][izqMtrx];
+            }
+            if (izqMtrx++ > derMtrx)
+            {
+                break;
+            }
         }
+        return matriz;
     }
 };
