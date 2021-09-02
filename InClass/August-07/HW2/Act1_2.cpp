499b5f7b4094ce0d623645bb25e8cd0d94e6763d
Axel3246 <hplaxel@gmail.com>
2021-09-02 12:20:51 -0500

    Updated Syntax

--- InClass/August-07/HW2/Act1_2.cpp
+++ InClass/August-07/HW2/Act1_2.cpp
@@ -11,29 +11,15 @@ numero de comparaciones y la posicion de el dato de un arreglo al ser ordenado
 
 using namespace std;
 
-/*
-void prlong(vector<long> &vect)
-{
-
-    for (long i = 0; i < vect.size(); i++)
-    {
-
-        cout << vect[i] << " ";
-    }
-    cout << endl;
-}
-*/
-
 // ! Funciones de Busqueda
 
 // * Funcion que busca un dato por medio de la busqueda secuencial y regresa el numero de comparaciones su posicion
 // ? Complejidad: Mejor caso: O(1), Peor caso: O(n)
-long busquedaSecuencial(vector<long> vect, long query)
-{
-    for (long i = 0; i < vect.size(); i++)
-    {
-        if (vect[i] == query)
-        {
+long busquedaSecuencial(vector<long> vect, long query){
+    
+    for (long i = 0; i < vect.size(); i++){
+        
+        if (vect[i] == query){
 
             return i;
         }
@@ -43,25 +29,26 @@ long busquedaSecuencial(vector<long> vect, long query)
 
 // * Funcion que busca un dato por medio de la busqueda binaria y regresa el numero de comparaciones su posicion
 // ? Complejidad: Mejor caso: O(1), Peor caso: O(log(n))
-long binarySearch(vector<long> vect, long query, long &comp)
-{
-    comp = 0;
+long binarySearch(vector<long> vect, long query, long &comp){
 
+    comp = 0;
     long inicio = 0, fin = vect.size() - 1, mitad;
-    while (inicio <= fin)
-    {
+
+    while (inicio <= fin){
+        
         mitad = (inicio + fin) / 2;
         comp++;
-        if (vect[mitad] == query)
-        {
+        if (vect[mitad] == query){
+
             return mitad;
         }
-        if (vect[mitad] > query)
-        { // El dato que busco es menor al central
+
+        if (vect[mitad] > query){ // El dato que busco es menor al central
+            
             fin = mitad - 1;
         }
-        else
-        { // El dato que busco es mayor al central
+        else{ // El dato que busco es mayor al central
+
             inicio = mitad + 1;
         }
     }
@@ -72,16 +59,18 @@ long binarySearch(vector<long> vect, long query, long &comp)
 
 // * Funcion que ordena un array / vector por el metodo de longercambio y regresa las comparaciones (mas el vector ordenado)
 // ? Complejidad: O(n^2)
-long sortIntercambio(vector<long> &vec)
-{
+long sortIntercambio(vector<long> &vec){
+
     long comp = 0, temp;
-    for (long i = 0; i < vec.size() - 1; i++)
-    {
-        for (long j = i + 1; j < vec.size(); j++)
-        {
+
+    for (long i = 0; i < vec.size() - 1; i++){
+
+        for (long j = i + 1; j < vec.size(); j++){
+
             comp++; // Medir las comparaciones
-            if (vec[i] > vec[j])
-            {
+
+            if (vec[i] > vec[j]){
+
                 temp = vec[i];
                 vec[i] = vec[j];
                 vec[j] = temp;
@@ -93,18 +82,21 @@ long sortIntercambio(vector<long> &vec)
 
 // * Funcion que ordena un array / vector por el metodo burbuja y regresa las comparaciones (mas el vector ordenado)
 // ? Complejidad: Mejor caso: O(n), Peor caso: O(n^2)
-long bubbleSort(vector<long> &vec)
-{
+long bubbleSort(vector<long> &vec){
+
     bool longerruptor = true;
     long temp, comp = 0;
-    for (long pasada = 0; pasada < vec.size() && longerruptor; pasada++)
-    {
+
+    for (long pasada = 0; pasada < vec.size() && longerruptor; pasada++){
+
         longerruptor = false;
-        for (long j = 0; j < vec.size() - 1 - pasada; j++)
-        {
+
+        for (long j = 0; j < vec.size() - 1 - pasada; j++){
+
             comp++;
-            if (vec[j + 1] < vec[j])
-            {
+
+            if (vec[j + 1] < vec[j]){
+
                 temp = vec[j];
                 vec[j] = vec[j + 1];
                 vec[j + 1] = temp;
@@ -116,39 +108,41 @@ long bubbleSort(vector<long> &vec)
 }
 
 // * Funcion que ordena un array / vector por el metodo mergeSort y regresa las comparaciones (mas el vector ordenado)
-// !!!!!!!!!! Complejidad: Posiblemente O(n)
-long merge(vector<long> &vec, long inicio, long mitad, long fin, long &comp)
-{
+// ? Complejidad: O(n)
+long merge(vector<long> &vec, long inicio, long mitad, long fin, long &comp){
+
     vector<long> aux(vec.size());
     long i = inicio, j = mitad + 1, k = inicio;
-    while (i <= mitad && j <= fin)
-    {
+
+    while (i <= mitad && j <= fin){
+
         comp++;
-        if (vec[i] <= vec[j])
-        {
+
+        if (vec[i] <= vec[j]){
+
             aux[k++] = vec[i++];
         }
-        else
-        {
+        else{
+
             aux[k++] = vec[j++];
         }
     }
-    if (i > mitad)
-    {
-        for (long a = j; a <= fin; a++)
-        {
+    if (i > mitad){
+
+        for (long a = j; a <= fin; a++){
+
             aux[k++] = vec[a];
         }
     }
-    else
-    {
-        for (long a = i; a <= mitad; a++)
-        {
+    else{
+
+        for (long a = i; a <= mitad; a++){
+
             aux[k++] = vec[a];
         }
     }
-    for (long a = inicio; a <= fin; a++)
-    {
+    for (long a = inicio; a <= fin; a++){
+
         vec[a] = aux[a];
     }
     return comp;
@@ -156,10 +150,10 @@ long merge(vector<long> &vec, long inicio, long mitad, long fin, long &comp)
 
 // * Funcion que ordena y divide en subarrays por recursividad para llevar a cabo mergeSort
 // ? Complejidad: Mejor caso: O(nlog(n)), Peor caso: O(nlog(n))
-void mergeSort(vector<long> &vec, long inicio, long fin, long &comp)
-{
-    if (inicio < fin)
-    {
+void mergeSort(vector<long> &vec, long inicio, long fin, long &comp){
+
+    if (inicio < fin){
+
         long mitad = (inicio + fin) / 2;
         mergeSort(vec, inicio, mitad, comp);
         mergeSort(vec, mitad + 1, fin, comp);
@@ -167,8 +161,8 @@ void mergeSort(vector<long> &vec, long inicio, long fin, long &comp)
     }
 }
 
-int main()
-{
+int main(){
+
     long nums, n, cont, queries, query;
     cin >> nums;
 
@@ -177,8 +171,8 @@ int main()
     // * vect3 == Ordenar con Merge
     vector<long> vect1, vect2, vect3;
 
-    for (long i = 0; i < nums; i++)
-    {
+    for (long i = 0; i < nums; i++){
+
         cin >> n;
         vect1.push_back(n);
         vect2.push_back(n);
@@ -202,8 +196,8 @@ int main()
 
     cin >> queries;
 
-    for (long j = 0; j < queries; j++)
-    {
+    for (long j = 0; j < queries; j++){
+        
         cin >> query;
         long cantCompBB = 0;
         long posBusBinaria = binarySearch(vect1, query, cantCompBB);
