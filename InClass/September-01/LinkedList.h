32e01ecca171b22c73feb36e5a032be0baa47fe4
Axel3246 <hplaxel@gmail.com>
2021-09-23 19:32:04 -0500

    LinkedList.h
    Added new methods. Solved syntax errors.

--- InClass/September-01/LinkedList.h
+++ InClass/September-01/LinkedList.h
@@ -4,18 +4,24 @@
 template <class T>
 class LinkedList
 {
-// * Atributos publicos
+    // * Atributos publicos
 public:
     LinkedList();
     ~LinkedList();
     void addFirst(T data);
     void addLast(T data);
+    bool add(T data, int pos);
     void deleteFirst();
     void deleteLast();
+    void del(int pos);
+    int deleteAll();
     bool isEmpty();
     int getSize();
+    T get(int pos);
+    T set(T data, int pos);
+    void print();
 
-// * Atributos privados
+    // * Atributos privados
 private:
     Node<T> *head;
     int size;
@@ -40,7 +46,7 @@ void LinkedList<T>::addFirst(T data)
 {
     // * Se crea un nuevo nodo, tomando en cuenta que
     // * head es de tipo nodo
-    head = new Node(data, head);
+    head = new Node<T>(data, head);
     size++;
 }
 
@@ -56,13 +62,43 @@ void LinkedList<T>::addLast(T data)
     else
     {
         Node<T> *curr = head;
-        while (curr->getNext != nullptr)
+        while (curr->getNext() != nullptr)
         {
             curr = curr->getNext();
         }
         curr->setNext(new Node<T>(data));
         size++;
     }
+}
+
+template <class T>
+// ? Complejidad: O(n)
+bool LinkedList<T>::add(T data, int pos)
+{
+
+    if (pos > size)
+    {
+        return false;
+    }
+    if (pos == 0)
+    {
+        addFirst(data);
+    }
+    else if (pos == size)
+    {
+        addLast(data);
+    }
+    else
+    {
+        Node<T> *curr = head;
+        for (int i = 1; i < pos; i++) // * Garantizar que entre pos - 1 veces
+        {
+            curr = curr->getNext();
+        }
+        curr->setNext(new Node<T>(data, curr->getNext()));
+        size++;
+    }
+    return true;
 }
 
 template <class T>
@@ -98,8 +134,52 @@ void LinkedList<T>::deleteLast()
         }
         delete curr->getNext(); // * Elimina la lista siguiente
         curr->setNext(nullptr); // * Establece NULL a la lista actual
-        size--; // * Reduce el contador del tamaño del linked list
+        size--;                 // * Reduce el contador del tamaño del linked list
     }
+}
+
+template <class T>
+// ? Complejidad: O(n)
+void LinkedList<T>::del(int pos)
+{
+
+    if (pos == 0)
+    {
+        deleteFirst();
+    }
+    else if (pos == size - 1)
+    {
+        deleteLast();
+    }
+    else
+    {
+        Node<T> *curr = head;
+        for (int i = 1; i < pos; i++)
+        {
+
+            curr = curr->getNext();
+        }
+        Node<T> *aux = curr->getNext();
+        curr->setNext(aux->getNext()); // curr->setNext(curr->getNext->getNext())
+        delete aux;
+        size--;
+    }
+}
+
+template <class T>
+// ? Complejidad: O(n)
+int LinkedList<T>::deleteAll()
+{
+    Node<T> *curr = head;
+    while (head != nullptr)
+    {
+        head = head->getNext();
+        delete curr;
+        curr = head;
+    }
+    int cant = size;
+    size = 0;
+    return cant;
 }
 
 template <class T>
@@ -114,4 +194,45 @@ template <class T>
 int LinkedList<T>::getSize()
 {
     return size;
+}
+
+template <class T>
+// ? Complejidad: O(n)
+T LinkedList<T>::get(int pos)
+{
+    Node<T> *curr = head;
+
+    for (int i = 1; i <= pos; i++)
+    {
+        curr = curr->getNext();
+    }
+    return curr->getData();
+}
+
+template <class T>
+// ? Complejidad: O(n)
+T LinkedList<T>::set(T data, int pos)
+{
+
+    Node<T> *curr = head;
+    for (int i = 1; i <= pos; i++)
+    {
+        curr = curr->getNext();
+    }
+    T dataAux = curr->getData();
+    curr->setData(data);
+    return dataAux;
+}
+
+template <class T>
+// ? Complejidad: O(1)
+void LinkedList<T>::print()
+{
+
+    Node<T> *curr = head;
+    while (curr != nullptr)
+    {
+        cout << curr->getData() << endl;
+        curr = curr->getNext();
+    }
 }
